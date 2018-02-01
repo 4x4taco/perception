988504889aa4ed4aa96dc79bea1ceaa78e167778
jojobilly <34203479+jojobilly@users.noreply.github.com>
2018-01-31 21:56:52 -0800

    Update segmentation.py

--- segmentation.py
+++ segmentation.py
@@ -15,16 +15,6 @@ def pcl_callback(pcl_msg):
     # TODO: PassThrough Filter
 
 
-# Callback function for your Point Cloud Subscriber
-def pcl_callback(pcl_msg):
-
-    # TODO: Convert ROS msg to PCL data
-
-    # TODO: Voxel Grid Downsampling
-
-    # TODO: PassThrough Filter
-
-
     # TODO: RANSAC Plane Segmentation
 
     # TODO: Extract inliers and outliers
@@ -39,20 +29,17 @@ def pcl_callback(pcl_msg):
     pcl_objects_pub.publish(pcl_msg)
     pcl_table_pub.publish(pcl_msg)
 
-    if __name__ == '__main__':
+if __name__ == '__main__':
 
-        # TODO: ROS node initialization
-        rospy.init_node('clustering', anonymous=True)
+    # TODO: ROS node initialization
+    rospy.init_node('clustering', anonymous=True)
 
-        # TODO: Create Subscribers
-        pcl_sub = rospy.Subscriber("/sensor_stick/point_cloud", pc2.PointCloud2, pcl_callback, queue_size=1)
+    # TODO: Create Subscribers
+    pcl_sub = rospy.Subscriber('/sensor_stick/point_cloud', pc2.PointCloud2, pcl_callback, queue_size=1)
 
-        # TODO: Create Publishers
-        pcl_objects_pub = rospy.Publisher("/pcl_objects", PointCloud2, queue_size=1)
-        pcl_table_pub = rospy.Publisher("/pcl_table", PointCloud2, queue_size=1)
-        # Initialize color_list
-        get_color_list.color_list = []
+    # TODO: Create Publishers
+    pcl_objects_pub = rospy.Publisher('/pcl_objects', PointCloud2, queue_size=1)
+    pcl_table_pub = rospy.Publisher('/pcl_table', PointCloud2, queue_size=1)
+    # Initialize color_list
+    get_color_list.color_list = []
 
-        # TODO: Spin while node is not shutdown
-        while not rospy.is_shutdown():
-            rospy.spin()
